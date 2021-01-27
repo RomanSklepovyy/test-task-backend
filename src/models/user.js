046ca07cb2e51c5430f2c46cf30c18db7d275935
Roman <romansklepovyy1@gmail.com>
2021-01-27 13:29:58 +0200

    create user model

---
+++ src/models/user.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true,
+    },
+  }],
+}, { timestamps: true });
+
+userSchema.virtual('orders', {
+  ref: 'Order',
+  localField: '_id',
+  foreignField: 'owner',
+});
+
+userSchema.methods.generateAuthToken = async function () {
+  // eslint-disable-next-line no-underscore-dangle
+  const token = jwt.sign({ _id: this._id.toString() }, process.env.JWT_SECRET);
+  this.tokens = this.tokens.concat({ token });
+  await this.save();
+
+  return token;
+};
+
+userSchema.methods.toJSON = function () {
+  const userObject = this.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
+const User = mongoose.model('User', userSchema);
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+
+  if (password !== user.password) {
+    throw new Error('Unable to login');
+  }
+
+  return user;
+};
+
+module.exports = User;
